510a90559d393d0b4803d100efbbf826f11e69b8
lpraat <pratissolil@gmail.com>
2020-03-01 15:42:53 +0100

    Added goal rl exp scripts

---
+++ goal_rl_short.sh
@@ -0,0 +1,76 @@
+# Short
+# GridGoal1
+nohup taskset -c 0-7 python main.py --seed 0 --domain "GridGoal1" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 1 --domain "GridGoal1" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 2 --domain "GridGoal1" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 3 --domain "GridGoal1" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 4 --domain "GridGoal1" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 5 --domain "GridGoal1" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 6 --domain "GridGoal1" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 6 --domain "GridGoal1" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+
+
+# GridGoal2
+nohup taskset -c 0-7 python main.py --seed 0 --domain "GridGoal2" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 1 --domain "GridGoal2" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 2 --domain "GridGoal2" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 3 --domain "GridGoal2" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 4 --domain "GridGoal2" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 5 --domain "GridGoal2" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 6 --domain "GridGoal2" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 7 --domain "GridGoal2" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+
+
+# GridGoal3
+nohup taskset -c 0-7 python main.py --seed 0 --domain "GridGoal3" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 1 --domain "GridGoal3" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 2 --domain "GridGoal3" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 3 --domain "GridGoal3" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 4 --domain "GridGoal3" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 5 --domain "GridGoal3" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 6 --domain "GridGoal3" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+nohup taskset -c 0-7 python main.py --seed 7 --domain "GridGoal3" --num_epochs 100 --num_expl_steps_per_train_loop 1200 --num_eval_steps_per_epoch 12000 --num_trains_per_train_loop 1200 --max_path_length 1200 &
+
+
+# AntEscape
+nohup taskset -c 0-7 python main.py --seed 0 --domain "AntEscape" --num_epochs 500 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 5000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 1 --domain "AntEscape" --num_epochs 500 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 5000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 2 --domain "AntEscape" --num_epochs 500 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 5000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 3 --domain "AntEscape" --num_epochs 500 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 5000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 4 --domain "AntEscape" --num_epochs 500 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 5000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 5 --domain "AntEscape" --num_epochs 500 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 5000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 6 --domain "AntEscape" --num_epochs 500 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 5000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 7 --domain "AntEscape" --num_epochs 500 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 5000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+
+
+# AntJump
+nohup taskset -c 0-7 python main.py --seed 0 --domain "AntJump" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 1 --domain "AntJump" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 2 --domain "AntJump" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 3 --domain "AntJump" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 4 --domain "AntJump" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 5 --domain "AntJump" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 6 --domain "AntJump" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 7 --domain "AntJump" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+
+
+# AntNavigate
+nohup taskset -c 0-7 python main.py --seed 0 --domain "AntNavigate" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 1 --domain "AntNavigate" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 2 --domain "AntNavigate" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 3 --domain "AntNavigate" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 4 --domain "AntNavigate" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 5 --domain "AntNavigate" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 6 --domain "AntNavigate" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+nohup taskset -c 0-7 python main.py --seed 7 --domain "AntNavigate" --num_epochs 1000 --num_expl_steps_per_train_loop 5000 --num_eval_steps_per_epoch 50000 --num_trains_per_train_loop 5000 --max_path_length 500 &
+
+
+# HumanoidUp
+nohup taskset -c 0-7 python main.py --seed 0 --domain "HumanoidUp" --num_epochs 2000 --num_expl_steps_per_train_loop 2000 --num_eval_steps_per_epoch 20000 --num_trains_per_train_loop 2000 --max_path_length 2000 &
+nohup taskset -c 0-7 python main.py --seed 1 --domain "HumanoidUp" --num_epochs 2000 --num_expl_steps_per_train_loop 2000 --num_eval_steps_per_epoch 20000 --num_trains_per_train_loop 2000 --max_path_length 2000 &
+nohup taskset -c 0-7 python main.py --seed 2 --domain "HumanoidUp" --num_epochs 2000 --num_expl_steps_per_train_loop 2000 --num_eval_steps_per_epoch 20000 --num_trains_per_train_loop 2000 --max_path_length 2000 &
+nohup taskset -c 0-7 python main.py --seed 3 --domain "HumanoidUp" --num_epochs 2000 --num_expl_steps_per_train_loop 2000 --num_eval_steps_per_epoch 20000 --num_trains_per_train_loop 2000 --max_path_length 2000 &
+nohup taskset -c 0-7 python main.py --seed 4 --domain "HumanoidUp" --num_epochs 2000 --num_expl_steps_per_train_loop 2000 --num_eval_steps_per_epoch 20000 --num_trains_per_train_loop 2000 --max_path_length 2000 &
+nohup taskset -c 0-7 python main.py --seed 5 --domain "HumanoidUp" --num_epochs 2000 --num_expl_steps_per_train_loop 2000 --num_eval_steps_per_epoch 20000 --num_trains_per_train_loop 2000 --max_path_length 2000 &
+nohup taskset -c 0-7 python main.py --seed 6 --domain "HumanoidUp" --num_epochs 2000 --num_expl_steps_per_train_loop 2000 --num_eval_steps_per_epoch 20000 --num_trains_per_train_loop 2000 --max_path_length 2000 &
+nohup taskset -c 0-7 python main.py --seed 7 --domain "HumanoidUp" --num_epochs 2000 --num_expl_steps_per_train_loop 2000 --num_eval_steps_per_epoch 20000 --num_trains_per_train_loop 2000 --max_path_length 2000 &
